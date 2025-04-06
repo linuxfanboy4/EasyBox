01f06982140be3a8feea4e18ac70c31391270fd9
linuxfanboy4 <imlinuxfanboy@outlook.com>
2025-04-06 18:10:56 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install requests
+curl -o easybox.py https://raw.githubusercontent.com/linuxfanboy4/EasyBox/refs/heads/main/src/easybox.py
+echo "EasyBox is installed! Run it by python3 easybox.py"
